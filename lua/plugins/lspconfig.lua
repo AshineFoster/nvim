65dd427dcab85571205fea5d59ed002510291251
AshineFoster <ashinefoster@gmail.com>
2021-10-25 03:19:17 -0500

    Change diagnostic symbols in the sign column (gutter)

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -85,10 +85,12 @@ end
 --   Warn:  ⚠ 
 --   Hint:  
 --   Info:   ⁱ
-local signs = { Error = '✘', Warn = '', Hint = '', Info = 'ⁱ'}
+
+local signs = { Error = "✘ ", Warning = " ", Hint = " ", Information = " " }
+
 for type, icon in pairs(signs) do
-	local hl = 'DiagnosticSign' .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 -- Setup CompletionItemKind symbols, see lua/lsp_kind.lua
